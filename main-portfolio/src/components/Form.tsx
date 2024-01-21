e1f9022a68e32ac6f33ff5aa0b14f7253afeecbe
Michael <mashireland@gmail.com>
2024-01-21 16:29:57 +0000

    Add Form Validation

--- main-portfolio/src/components/Form.tsx
+++ main-portfolio/src/components/Form.tsx
@@ -1,40 +1,57 @@
-import { FormEvent, useRef } from "react";
+import { useForm, FieldValues } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import Textarea from "./Textarea";
+import Input from "./Input";
+
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+  email: z
+    .string()
+    .min(6, { message: "Email must be at least 6 characters" })
+    .email("This is not a valid email."),
+
+  message: z
+    .string()
+    .min(10, { message: "Message must be at least 10 characters" })
+    .max(300, { message: "Message must be at less than 300 characters" }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ mode: "onTouched", resolver: zodResolver(schema) });
 
-  const person = { name: "", age: 0 };
+  const [isSubmitting, setSubmitting] = useState(false);
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-
-    if (nameRef.current) {
-      person.name = nameRef.current.value;
-    }
-
-    if (ageRef.current) {
-      person.age = parseInt(ageRef.current.value);
+  const onSubmit = async (data: FieldValues) => {
+    setSubmitting(true);
+    try {
+      console.log(data);
+      //wait 3 seconds
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+    } finally {
+      setSubmitting(false);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
-        </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
-      </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input name="Name" type="text" register={register} errors={errors} />
+      <Input name="Email" type="email" register={register} errors={errors} />
+      <Textarea name="Message" register={register} errors={errors} />
 
-      <div className="mb-3">
-        <label htmlFor="age" className="form-label">
-          Age
-        </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
-      </div>
-      <button type="submit" className="btn btn-primary flex">
-        Submit
+      <button
+        disabled={!isValid || isSubmitting}
+        type="submit"
+        className="btn btn-primary flex"
+      >
+        {isSubmitting ? "Submitting..." : "Submit"}
       </button>
     </form>
   );
