53f227bfe283213e30de52f93ace7bc719cbd3ea
Michael-Grinnell <79202699+Michael-Grinnell@users.noreply.github.com>
2024-02-19 16:47:50 +0000

    Add ImageCompnent

--- main-portfolio/src/App.tsx
+++ main-portfolio/src/App.tsx
@@ -1,16 +1,11 @@
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import NavbarMain from "./features/NavbarMain";
 import TextContentSection from "./components/layout/TextContentSection";
-import BentoBox from "./components/layout/BentoBox";
-import BentoGridItem from "./components/layout/BentoGirdItem";
-import logo from "@images/logo.png";
-import c_logo from "@images/Logos/c++.png";
-import git_logo from "@images/Logos/git.png";
-import php_logo from "@images/Logos/php.png";
-import python_logo from "@images/Logos/python.png";
-import react_logo from "@images/Logos/react_js.png";
+import ImageComponent from "./components/ui/ImageComponent";
 
 function App() {
+
+
   return (
     <Container>
       {/* <ParticleBackground /> */}
@@ -30,74 +25,7 @@ function App() {
       {/* <Tags options={["C++", "Python", "Web Development"]}></Tags> */}
 
       <Row>
-        <BentoBox>
-          <BentoGridItem rowSpan={1} colSpan={8}>
-            <img
-              src={php_logo}
-              width="120"
-              height="120"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </BentoGridItem>
-          <BentoGridItem rowSpan={1} colSpan={2}>
-            <img
-              src={c_logo}
-              width="120"
-              height="120"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </BentoGridItem>
-          <BentoGridItem rowSpan={2} colSpan={4}>
-            <Row>
-              <Card
-                style={{ width: "100%", maxWidth: "18rem", margin: "auto" }}
-              >
-                <Card.Img variant="top" src={logo} />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Row>
-          </BentoGridItem>
-          <BentoGridItem rowSpan={1} colSpan={2}>
-            <img
-              src={git_logo}
-              width="120"
-              height="120"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </BentoGridItem>
-          <BentoGridItem rowSpan={1} colSpan={2} offset={true}>
-            {" "}
-          </BentoGridItem>
-          <BentoGridItem rowSpan={1} colSpan={4}>
-            <img
-              src={python_logo}
-              width="120"
-              height="120"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </BentoGridItem>
-          <BentoGridItem rowSpan={1} colSpan={4}>
-            <img
-              src={react_logo}
-              width="120"
-              height="120"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </BentoGridItem>
-          {/* Add more items as needed */}
-        </BentoBox>
+        <ImageComponent name="koala" />
       </Row>
     </Container>
   );
---
+++ main-portfolio/src/data/assets.ts
@@ -0,0 +1,8 @@
+// Images
+import koalaImage from '@images/koala.jpeg';
+
+export const images = {
+    koala: koalaImage,
+};
+
+export type ImageName = keyof typeof images;
