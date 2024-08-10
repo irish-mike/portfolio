1621eecc696fbca96e85615e08d3c281b51e36db
Michael Grinnell <michael@providentcrm.com>
2024-08-10 18:31:25 +0200

    Update Social Links

--- portfolio_front_end/src/components/Atoms/Icons/SocialIconGroup.tsx
+++ portfolio_front_end/src/components/Atoms/Icons/SocialIconGroup.tsx
@@ -1,19 +1,27 @@
-import { Stack } from 'react-bootstrap';
-import { SocialIcon } from 'react-social-icons/component';
-import 'react-social-icons/linkedin';
-import 'react-social-icons/github';
-import 'react-social-icons/soundcloud';
-import 'react-social-icons/youtube';
+import { Stack } from "react-bootstrap";
+import { SocialIcon } from "react-social-icons/component";
+import "react-social-icons/linkedin";
+import "react-social-icons/github";
+import "react-social-icons/soundcloud";
+import "react-social-icons/youtube";
 
 const SocialIconGroup = () => {
-    return (
-        <Stack direction="horizontal" gap={3}>
-            <div className="p-1"><SocialIcon url="https://www.linkedin.com" /></div>
-            <div className="p-1"><SocialIcon url="https://github.com" /></div>
-            <div className="p-1"><SocialIcon url="https://www.soundcloud.com" /></div>
-            <div className="p-1"><SocialIcon url="https://www.youtube.com" /></div>
-        </Stack>
-    );
-}
+  return (
+    <Stack direction="horizontal" gap={3}>
+      <div className="p-1">
+        <SocialIcon url="https://www.linkedin.com/in/-michael-grinnell" target="_blank" />
+      </div>
+      <div className="p-1">
+        <SocialIcon network="github" href="https://bitbucket.org/michael_grinnell" target="_blank" />
+      </div>
+      <div className="p-1">
+        <SocialIcon url="https://soundcloud.com/mikegireland" target="_blank" />
+      </div>
+      <div className="p-1">
+        <SocialIcon url="https://www.youtube.com/@digitaldeveloper4803" target="_blank" />
+      </div>
+    </Stack>
+  );
+};
 
 export default SocialIconGroup;
