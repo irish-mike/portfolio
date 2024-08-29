d018c398547af84b7b8c19a93c46a06b9095409f
Michael Grinnell <michael@providentcrm.com>
2024-08-29 15:57:20 +0100

    Fix menu on mobile

--- portfolio_front_end/src/components/Organisms/Layouts/NavbarMain.tsx
+++ portfolio_front_end/src/components/Organisms/Layouts/NavbarMain.tsx
@@ -1,44 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsHouse } from "react-icons/bs";
 import { CgPacman } from "react-icons/cg";
-import { IoGameControllerOutline, IoMailOutline } from "react-icons/io5";
+import { IoGameControllerOutline } from "react-icons/io5";
 import { SlBubbles } from "react-icons/sl";
 import { Link } from "react-router-dom";
 import logo from "@assets/images/logo_dark.png";
-import { Button, Form, Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import ColorModeSwitch from "../../../features/ColorModeSwitch";
-import { ContactFormModal } from "@components";
 
 const NavbarMain = () => {
+  // State to control the collapse of the Navbar
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => setExpanded(!expanded);
+  const handleNavLinkClick = () => setExpanded(false); // Close on Nav.Link click
+
   return (
-    <Navbar expand="md" className="justify-content-center">
-      <Navbar.Brand href="/" className="fixed-width m-0">
-        <div className="logo-container">
-          <img src={logo} width="60" height="60" className="d-inline-block align-top" alt="React Bootstrap logo" />
-        </div>
-      </Navbar.Brand>
+    <Navbar expand="lg" expanded={expanded} onToggle={handleToggle} className="px-3 d-flex justify-content-between align-items-center">
+      <div className="fixed-width">
+        <ColorModeSwitch label="Developer Mode" />
+      </div>
 
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
 
-      <Navbar.Collapse id="basic-navbar-nav" className="w-100">
-        <Nav className="w-100 justify-content-center">
-          <NavItem to="/" label="Home" icon={<BsHouse className="m-1" />} />
-          <NavItem to="/posts/portfolio" label="Portfolio" icon={<IoGameControllerOutline className="m-1" />} />
-          <NavItem to="/posts/blog" label="Blog" icon={<SlBubbles className="m-1" />} />
-          <NavItem to="/about" label="About" icon={<CgPacman className="m-1" />} />
-          <ContactFormModal
-            trigger={
-              <Button variant="link" className="nav-link p-3 border-bottom-mobile">
-                Contact <IoMailOutline className="m-1" />
-              </Button>
-            }
-          />
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+        <Nav className="mx-auto text-center">
+          <NavItem to="/" label="Home" icon={<BsHouse className="m-1" />} onClick={handleNavLinkClick} />
+          <NavItem to="/posts/portfolio" label="Portfolio" icon={<IoGameControllerOutline className="m-1" />} onClick={handleNavLinkClick} />
+          <NavItem to="/posts/blog" label="Blog" icon={<SlBubbles className="m-1" />} onClick={handleNavLinkClick} />
+          <NavItem to="/about" label="About" icon={<CgPacman className="m-1" />} onClick={handleNavLinkClick} />
         </Nav>
-
-        <Form className="fixed-width p-3">
-          <ColorModeSwitch label="Developer Mode" />
-        </Form>
       </Navbar.Collapse>
+
+      <div className="d-none d-lg-block fixed-width">
+        <Navbar.Brand href="/" style={{ display: "flex", justifyContent: "flex-end", width: "100%" }}>
+          <img src={logo} width="60" height="60" alt="React Bootstrap logo" />
+        </Navbar.Brand>
+      </div>
     </Navbar>
   );
 };
@@ -49,10 +47,11 @@ interface NavItemProps {
   to: string;
   label: string;
   icon: React.ReactNode;
+  onClick: () => void; // Add onClick prop to handle click events
 }
 
-const NavItem = ({ to, label, icon }: NavItemProps) => (
-  <Link to={to} className="nav-link p-3 border-bottom-mobile">
+const NavItem = ({ to, label, icon, onClick }: NavItemProps) => (
+  <Nav.Link as={Link} to={to} onClick={onClick} className="p-2 d-flex align-items-center justify-content-center nav-link p-3 border-bottom-mobile">
     {label} {icon}
-  </Link>
+  </Nav.Link>
 );
--- portfolio_front_end/src/pages/Layout.tsx
+++ portfolio_front_end/src/pages/Layout.tsx
@@ -1,19 +1,17 @@
 import { Container } from "react-bootstrap";
 
-import { Footer, NavbarMain, TransitionWrapper } from "@components";
+import { Footer, TransitionWrapper, NavbarMain } from "@components";
 
 // import ParticleBackground from "../features/ParticleBackground";
 
 const Layout = () => {
   return (
-    <>
-      <Container>
-        {/* <ParticleBackground /> */}
-        <NavbarMain />
-        <TransitionWrapper />
-        <Footer />
-      </Container>
-    </>
+    <Container fluid className="p-0">
+      {/* <ParticleBackground /> */}
+      <NavbarMain />
+      <TransitionWrapper />
+      <Footer />
+    </Container>
   );
 };
 
