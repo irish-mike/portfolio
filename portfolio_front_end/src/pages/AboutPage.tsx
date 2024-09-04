7d44de67e1ab8b82d57eb4028e65ecfcc0b2a7dd
Michael Grinnell <michael@providentcrm.com>
2024-09-04 13:27:16 +0100

    Update About Content

--- portfolio_front_end/src/pages/AboutPage.tsx
+++ portfolio_front_end/src/pages/AboutPage.tsx
@@ -5,34 +5,43 @@ import { SocialIconGroup } from "@components"; // Adjust the import based on you
 const AboutPage = () => {
   return (
     <Container className="section-inner my-5">
+      {/* Header Section */}
       <header className="text-center mb-5">
-        <Row className="align-items-end justify-content-between m-0 p-0">
+        <Row className="align-items-end justify-content-between">
           <Col xs={12} md="auto" className="text-md-start text-center mb-2 mb-md-0">
             <h1 className="mb-0">Who am I?</h1>
           </Col>
           <Col xs={12} md="auto" className="text-md-end text-center">
-            <h4 className="text-muted h5 fw-light mb-0">Realist, Pragmatic, Enjoys life and big nerd</h4>
+            <h4 className="text-muted h5 fw-light mb-0">Curious, Driven, Creative, Nerd</h4>
           </Col>
         </Row>
         <hr className="w-100 my-1" />
       </header>
 
+      {/* Image and Text Section */}
       <section className="mb-3 pt-4">
         <Row>
-          <Col className="pb-4" lg={5}>
+          <Col lg={5} className="pt-2 text-center text-lg-start">
             <Image className="profile-image" src={michael_walking} rounded fluid alt="Michael Grinnell" />
           </Col>
 
-          <Col lg={{ span: 6, offset: 1 }}>
-            <Col md={12}>
-              <h4 className="text-background">Hello and welcome to my corner of the internet!</h4>
-              <p className="text-background">
-                Hey there! I'm Michael Grinnell, a full stack developer who's passionate about crafting clean, efficient code and building applications that perform seamlessly. Over the years, I've honed my skills through various projects, ranging from VST plugin development to music production, and
-                even setting up an Ubuntu-based home media server.
-              </p>
-              <p className="text-background">When I'm not coding, you can find me indulging in my hobbies. I love producing music, playing the piano (I've reached RIAM Grade 2), developing VST plugins, and diving into the latest tech trends and frameworks.</p>
-            </Col>
-            <Col md={12} className="d-flex justify-content-center align-items-center my-5">
+          <Col lg={7}>
+            <h4 className="text-background my-2">Dia duit! Hello! Bienvenido! Salut! Willkommen! Ciao!</h4>
+            <p className="text-background">
+              I’m Michael, a curious software developer with a passion for technology and problem-solving. With a BSc in Computing in Games Development and an MSc in Computing in Music Technology, both completed with top honors and recognition through the President’s Prize for Academic Excellence,
+              I’m now pursuing a master’s in Artificial Intelligence, aiming for completion by 2026.
+            </p>
+            <p className="text-background">
+              I’m currently working as a full-stack developer at{" "}
+              <a href="https://www.providentcrm.com/" target="_blank">
+                Provident CRM
+              </a>
+              , where I collaborate closely with clients to deliver custom software solutions. Proficient in C++, Python, PHP, and JavaScript, I’m a big fan of coding principles like KISS (Keep It Simple, Stupid) and DRY (Don’t Repeat Yourself). These principles, among others, ensure that the
+              solutions I create are both effective and scalable. I thrive in agile environments where I prioritize clear communication with both customers and team members, which I’ve found to be key to a successful project.{" "}
+            </p>
+
+            {/* Follow Me Section */}
+            <div className="d-flex justify-content-center align-items-center my-5">
               <div className="text-center">
                 <h3 className="text-background">Follow Me</h3>
                 <hr className="my-3" />
@@ -40,22 +49,26 @@ const AboutPage = () => {
                   <SocialIconGroup />
                 </div>
               </div>
-            </Col>
+            </div>
           </Col>
         </Row>
       </section>
 
+      {/* Music and Technology Section */}
       <section className="py-2">
         <Row>
           <Col md={12}>
-            <h4 className="text-background">Some more stuff about me!</h4>
+            <h4 className="text-background">Exploring the World Through Music and Technology</h4>
             <p className="text-background">
-              Hey there! I'm Michael Grinnell, a full stack developer who's passionate about crafting clean, efficient code and building applications that perform seamlessly. Over the years, I've honed my skills through various projects, ranging from VST plugin development to music production, and
-              even setting up an Ubuntu-based home media server.
+              I believe in balancing work with my passions, and music is at the top of the list. I’m a Grade 2 classical pianist, I regularly produce music, and I also DJ. For two years, I held a DJ slot on{" "}
+              <a href="https://www.phever.ie/" target="_blank">
+                Phever Media
+              </a>
+              , a radio station in Ireland, where I was able to share my love for music with a wide audience. This combination of creativity and technical skill gives me a unique perspective, especially when working on audio programming or music-related projects.
             </p>
             <p className="text-background">
-              I hold dual citizenship in the US and Ireland and currently reside in the beautiful city of Valencia, Spain. My Spanish is at a B1 level, and I'm actively working towards fluency. I'm always open to new opportunities, especially if they offer a chance to relocate and explore new
-              cultures.
+              When I’m not coding or making music, you’ll likely find me traveling and exploring new places while learning something new. I live in Valencia, Spain, where I’m actively learning Spanish and aiming to have conversational fluency by next year. Whether I’m diving into new technology,
+              taking on a personal project (like building a home server), or keeping up with the latest music trends, I’m driven by curiosity and a love for creating meaningful things.{" "}
             </p>
           </Col>
         </Row>
--- portfolio_front_end/src/pages/HomePage.tsx
+++ portfolio_front_end/src/pages/HomePage.tsx
@@ -5,9 +5,9 @@ const HomePage = () => {
     <>
       <TextContentSection title={"Michael Grinnell"} description={"Experienced software engineer with emphasis on games and audio programming, combining creativity and technical skills."} />
       <AboutSection title={"About Me"}>
-        <p className="text-background">Being a software developer isn't just about writing code; it's about creating elegant solutions, solving complex problems and bringing innovative ideas to life!</p>
-        <p className="text-background">This what I do, whether I'm building a full-scale application or perfecting a recipe for the best taco you've ever tasted! The sense of achievement from creating something great is what drives me every day.</p>
-        <p className="text-background">I am currently located in Valencia, Spain, am proficient in Spanish at a B1 level, aiming for fluency. I have dual US and Irish citizenship and am open to relocation for the right opportunity.</p>
+        <p className="text-background">Being a software developer isn't just about writing code; it's about creating solutions, solving problems, and bringing ideas to life!</p>
+        <p className="text-background">Whether I’m developing a full-scale application or crafting the perfect taco recipe, the sense of accomplishment that comes from turning an idea into reality is what drives me.</p>
+        <p className="text-background">I am currently located in Valencia, Spain, am proficient in Spanish at a B1 level, and am aiming for fluency. I have dual US and Irish citizenship and am open to relocation for the right opportunity.</p>
       </AboutSection>
 
       <FeaturedProjectsSection />
